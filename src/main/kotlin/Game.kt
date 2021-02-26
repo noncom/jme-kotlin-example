babef9992ff6f7c97dcc22044474bf7059f4db14
Alex Fowler <alex.murataev@gmail.com>
2021-02-26 15:38:47 +0300

    initial commit

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,21 @@
+import com.jme3.app.SimpleApplication
+import com.jme3.material.Material
+import com.jme3.math.ColorRGBA
+
+import com.jme3.scene.Geometry
+import com.jme3.scene.shape.Box
+
+
+class Game : SimpleApplication() {
+
+    override fun simpleInitApp() {
+        val b = Box(1f, 1f, 1f)
+        val geom = Geometry("Box", b)
+
+        val mat = Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md")
+        mat.setColor("Color", ColorRGBA.Blue)
+        geom.material = mat
+
+        rootNode.attachChild(geom)
+    }
+}
---
+++ src/main/kotlin/Launcher.kt
@@ -0,0 +1,9 @@
+object Launcher {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val game = Game()
+        game.start()
+    }
+
+}
