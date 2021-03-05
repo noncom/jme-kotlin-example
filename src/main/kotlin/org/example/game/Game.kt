0dcff6397a6cc504a3d0a699f65d5b9c602d0c17
Alex Fowler <alex.murataev@gmail.com>
2021-03-05 11:43:04 +0300

    add gitignore

--- src/main/kotlin/org/example/game/Game.kt
+++ src/main/kotlin/org/example/game/Game.kt
@@ -1,8 +1,13 @@
+package org.example.game
+
 import com.jme3.app.SimpleApplication
+import com.jme3.light.SpotLight
 import com.jme3.material.Material
 import com.jme3.math.ColorRGBA
+import com.jme3.scene.CameraNode
 
 import com.jme3.scene.Geometry
+import com.jme3.scene.LightNode
 import com.jme3.scene.shape.Box
 
 
@@ -12,9 +17,18 @@ class Game : SimpleApplication() {
         val b = Box(1f, 1f, 1f)
         val geom = Geometry("Box", b)
 
+        val cameraNode = CameraNode("main", camera)
+        val light = SpotLight()
+
+        val ln = LightNode("spot", light)
+        cameraNode.attachChild(ln)
+
         val mat = Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md")
         mat.setColor("Color", ColorRGBA.Blue)
         geom.material = mat
+
+        flyCam.isEnabled = false
+
 
         rootNode.attachChild(geom)
     }
--- src/main/kotlin/org/example/game/Launcher.kt
+++ src/main/kotlin/org/example/game/Launcher.kt
@@ -1,3 +1,5 @@
+package org.example.game
+
 object Launcher {
 
     @JvmStatic
